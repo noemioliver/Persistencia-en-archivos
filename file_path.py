0d9324671986a3f8b77cd3b39f4cc36538e61d01
noemioliver <noemioliverborja.com>
2026-01-22 12:34:05 +0100

    i

--- file_path.py
+++ file_path.py
@@ -1,27 +1,25 @@
 import os
 
-#write
-file = open("users.txt", "w")
-file.write("COCACOLA\n")
-file.close()
+def write_file(archivo):
+    file = open(archivo, "w")
+    file.write("INICIO")
+    file.close()
 
+def read_file(archivo):
+    file = open(archivo, "r")
+    print(file.read())
+    file.close()
 
-#read
-file = open("users.txt", "r")
-content = file.read()
-print(content)
-file.close()
+def update_file(archivo):
+    file = open(archivo, "w")
+    file.write("ACTUALIZADO")
+    file.close()
 
+def delete_file(archivo):
+    os.remove(archivo)
 
-#update
-file = open("users.txt", "r")
-content = file.read()
-print(content)
-file.close()
-
-file = open("users.txt", "w")
-content = file.write(COCACOLITA\n)
-file.close()
-
-#delete
-#os.remove("users.txt")
+write_file("users.txt")
+read_file("users.txt")
+update_file("users.txt")
+read_file("users.txt")
+# delete_file("users.txt")
---
+++ tests/tests_file.py
@@ -0,0 +1,11 @@
+def test_crear_archivo():
+    pass
+
+def test_leer_archivo():
+    pass
+
+def test_actualizar_archivo():
+    pass
+
+def test_agregar_texto():
+    pass
